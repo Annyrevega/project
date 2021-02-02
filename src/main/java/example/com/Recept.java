3b44635bcf6aaa8bf565ed519f3316d6230703f0
anutarevega <anutkarevega@gmail.com>
2021-02-02 11:57:03 +0300

    Second commit

---
+++ src/main/java/example/com/Recept.java
@@ -0,0 +1,17 @@
+package example.com;
+
+public class Recept {
+    private Product product;
+
+
+
+    public Recept(Product product) {
+        System.out.println("Рецепт создан");
+        this.product = product;
+    }
+
+
+public void may(){
+    System.out.println("!kk");
+}
+}
--- src/main/java/example/com/test.java
+++ src/main/java/example/com/test.java
@@ -1,4 +1,12 @@
 package example.com;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class test {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Recept recept = context.getBean("recept", Recept.class);
+        recept.may();
+        context.close();
+    }
 }
